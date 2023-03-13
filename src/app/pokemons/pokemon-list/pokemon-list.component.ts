a5f7cd70ac21000a43a3b2f02be110f6794f0c01
Nadav Ben Hur <nadavb@moveo.co.il>
2023-03-13 12:08:58 +0200

    cleanup

--- src/app/pokemons/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemons/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouteParamsService } from 'src/app/services/route-params.service';
 import { IPokemon } from '../models/pokemon.model';
 
