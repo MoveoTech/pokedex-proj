f5be662b581983e6755376a707cee02e1f5dd672
Nadav Ben Hur <nadavb@moveo.co.il>
2023-03-14 17:45:33 +0200

    Remove env file

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    pokemonApiUrl : 'https://pokeapi.co/api/v2/pokemon/',
-    loginToken : 'demo@skills.co.il',
-    loginStorageKey : 'pokemon-login',
-    searchLogKey : 'pokemon-search-history',
-
-}
